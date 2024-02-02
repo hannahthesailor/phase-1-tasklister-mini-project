47f86d24e2543e30145326b0f429312c7e1d5768
Hannah Bradley <hannahthesailor@yahoo.com>
2024-02-02 13:58:09 -0500

    phase-1-tasklister-mini-project.md

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    const form = document.getElementById("create-task-form");
+    const todoList = document.getElementById("tasks");
+  
+    form.addEventListener("submit", function(event) {
+      event.preventDefault();
+  
+      const taskInput = document.getElementById("new-task-description");
+      const taskDescription = taskInput.value;
+  
+      const taskItem = document.createElement("li");
+      taskItem.textContent = taskDescription;
+      todoList.appendChild(taskItem);
+  
+      taskInput.value = "";
+    });
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+});
 });
